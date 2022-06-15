e0644178fc151dff88e4c84510f728943c501dfa
rquintg <ricardoqg41@icloud.com>
2022-06-14 22:18:53 -0500

    :sparkles: se crea tabla y se renderiza los datos de Tipos de estado

--- src/components/estado/Estado.js
+++ src/components/estado/Estado.js
@@ -1,12 +1,16 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import {obtenerTodos} from "../../services/EstadoService";
 
 export default function Estado() {
 
+
+    const [estados, setEstados] = useState([]);
+
     useEffect( () => {
         const getEstados = () => {
             obtenerTodos().then(r => {
-                console.log(r)
+                console.log(r);
+                setEstados(r.data.tasks);
             }).catch(e => {
                 console.log(e)
             })
@@ -17,6 +21,56 @@ export default function Estado() {
 
 
     return (
-        <div>Estado</div>
+        <div className="container"><h1 className='text-center'>Estado </h1>
+            <button type="button"
+                    className="btn btn-primary">
+                <i className="fa-solid fa-file-circle-plus"></i>
+            </button>
+
+            <div className="table-responsive">
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th scope="col"><i className="fa-solid fa-users-viewfinder"></i></th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Estado</th>
+                        <th scope="col">Creacion</th>
+                        <th scope="col">Actualización</th>
+                        <th scope="col">Acciones</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            estados.map((est, index) => {
+                                return(
+                                    <tr>
+                                        <th scope="row">{index + 1}</th>
+                                        <td>{est.nombre}</td>
+                                        <td>{est.estado}</td>
+                                        <td>{est.createdAt}</td>
+                                        <td>{est.updatedAt}</td>
+                                        <td>
+                                            <button type="button"
+                                                    className="btn btn-outline-success">
+                                                <i  className="fa-solid fa-pen-to-square"></i>
+                                            </button>
+
+                                            <button type="button"
+                                                    className="btn btn-outline-danger">
+                                                <i className="fa-solid fa-trash"></i>
+                                            </button>
+
+                                        </td>
+                                    </tr>
+                                );
+                            })
+                        }
+
+                    </tbody>
+                </table>
+            </div>
+
+
+        </div>
     )
 }
