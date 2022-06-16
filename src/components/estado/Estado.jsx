2f256f66c5215856b15251422e79ea26ec648b6a
rquintg <ricardoqg41@icloud.com>
2022-06-16 02:43:33 -0500

    :white_check_mark: Se actualizan funciones y metodos

--- src/components/estado/Estado.jsx
+++ src/components/estado/Estado.jsx
@@ -1,5 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import {obtenerTodos, guardar, editarporId} from "../../services/EstadoService";
+import TablaModulos from "../userinterface/TablaModulos";
+import ModalWindow from "./ModalWindow";
 
 export default function Estado() {
 
@@ -27,6 +29,7 @@ export default function Estado() {
     }, []);
 
     const changeEstado =  e => {
+        e.preventDefault();
         setEstado({
             ...estado,
             [e.target.name]: e.target.value
@@ -46,6 +49,7 @@ export default function Estado() {
         }else{
             guardarEstado();
         }
+        closeModal();
 
     }
 
@@ -112,140 +116,31 @@ export default function Estado() {
     }
 
     return (
-        <div className="container"><h1 className='text-center'>Estado </h1>
-            <button type="button"
-                    className="btn btn-primary"
-                    data-bs-toggle="modal"
-                    data-bs-target="#exampleModal"
-                    onClick={closeModal}>
-                <i className="fa-solid fa-file-circle-plus"></i>
-            </button>
+        <div className="container">
+            <h1 className='text-center'>Estado </h1>
+                <button type="button"
+                        className="btn btn-primary"
+                        data-bs-toggle="modal"
+                        data-bs-target="#exampleModal"
+                        onClick={closeModal}>
+                    <i className="fa-solid fa-file-circle-plus"></i>
+                </button>
 
-            <div className="table-responsive">
-                <table className="table">
-                    <thead>
-                    <tr>
-                        <th scope="col"><i className="fa-solid fa-users-viewfinder"></i></th>
-                        <th scope="col">Nombre</th>
-                        <th scope="col">Estado</th>
-                        <th scope="col">Creacion</th>
-                        <th scope="col">Actualización</th>
-                        <th scope="col">Acciones</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {
-                        estados.map((est, index) => {
-                            const date1 =  new Date(est.createdAt);
-                            const date2 =  new Date(est.updatedAt);
-                            const creacion = date1.getDay()+"/"+date1.getMonth()+"/"+date1.getFullYear();
-                            const actualizacion = date2.getDay()+"/"+date2.getMonth()+"/"+date2.getFullYear();
+                <TablaModulos componentes={estados}
+                              openEditById={openEditById}
+                />
 
-                            return(
-                                <tr key={est._id}>
-                                    <th scope="row">{index + 1}</th>
-                                    <td>{est.nombre}</td>
-                                    <td>{est.estado ? 'Activo' : 'Inactivo'}</td>
-                                    <td>{creacion}</td>
-                                    <td>{actualizacion}</td>
-                                    <td>
-                                        <button type="button"
-                                                className="btn btn-outline-success"
-                                                data-bs-toggle="modal"
-                                                data-bs-target="#exampleModal"
-                                                data={est._id}
-                                                onClick={e => openEditById(e)}>
-                                            <i  className="fa-solid fa-pen-to-square"></i>
-                                        </button>
+                <ModalWindow
+                    estado={estado}
+                    loading={loading}
+                    closeModal={closeModal}
+                    hidden={hidden}
+                    changeEstado={changeEstado}
+                    error={error}
+                    add={add}
+                />
 
-                                        <button type="button"
-                                                className="btn btn-outline-danger">
-                                            <i className="fa-solid fa-trash"></i>
-                                        </button>
 
-                                    </td>
-                                </tr>
-                            );
-                        })
-                    }
-
-                    </tbody>
-                </table>
-            </div>
-
-            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel"
-                 aria-hidden="true">
-                <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">{estado._id ? 'Editar estado' : 'Ingrese nuevo estado'}</h5>
-                            &nbsp;&nbsp;
-                            {
-                                (loading &&
-                                    <div className="spinner-grow text-success" role="status">
-                                        <span className="visually-hidden">Loading...</span>
-                                    </div>
-                                )
-                            }
-                            <button type="button"
-                                    className="btn-close"
-                                    data-bs-dismiss="modal"
-                                    aria-label="Close"
-                                    onClick={closeModal}>
-
-                            </button>
-                        </div>
-                        <div className="modal-body">
-                            <form onSubmit={add}>
-                                <input type={hidden} name="_id" value={estado._id}></input>
-                                <div className="mb-3">
-                                    <label htmlFor="recipient-name" className="col-form-label">Nombre:</label>
-                                    <input required
-                                           disabled={estado._id ? true : false}
-                                           readOnly={estado._id ? true : false}
-                                           value={estado.nombre}
-                                           name="nombre"
-                                           type="text"
-                                           className="form-control"
-                                           onChange={changeEstado}/>
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="message-text" className="col-form-label">Estado:</label>
-                                    <select required
-                                            className="form-select"
-                                            aria-label="Default select example"
-                                            value={estado.estado}
-                                            name="estado"
-                                            onChange={changeEstado}>
-                                        <option value={true}>Activo</option>
-                                        <option value={false}>Inactivo</option>
-                                    </select>
-                                </div>
-
-                                <div className="modal-footer">
-                                    <div className={error ? 'alert alert-danger' : 'd-none'} role="alert">
-                                        Ha ocurrido un error!
-                                    </div>
-                                    <button type="button"
-                                            className="btn btn-secondary"
-                                            data-bs-dismiss="modal"
-                                            onClick={closeModal}>
-                                        <i className="fa-solid fa-arrow-right-from-bracket"></i> Cancelar
-                                    </button>
-                                    <button type="submit"
-                                            className="btn btn-primary"
-                                            data-bs-dismiss="modal">
-
-                                        <i  className="fa-solid fa-floppy-disk"></i> Enviar
-                                    </button>
-                                </div>
-
-                            </form>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
 
         </div>
     )
