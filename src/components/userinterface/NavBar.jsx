7f0d47f9c3598417fa5ebe65e99086d22b87db0f
rquintg <ricardoqg41@icloud.com>
2022-06-20 17:44:27 -0500

    :white_check_mark: Se configura Navbar y rutas

--- src/components/userinterface/NavBar.jsx
+++ src/components/userinterface/NavBar.jsx
@@ -3,9 +3,13 @@ import {NavLink} from "react-router-dom";
 
 export default function NavBar() {
     return (
-        <nav className="navbar navbar-expand-lg bg-primary">
+        <nav className="navbar navbar-expand-lg bg-danger">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">Invetario</a>
+                <a className="navbar-brand" href="/">
+                    <img className="img-fluid"
+                         src="https://www.sucesosmetropolitanos.com/wp-content/uploads/2020/09/logo-IU-660x332@2x.jpeg"
+                         alt="Logo" width="150" height="60" />
+                </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                         data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                         aria-expanded="false" aria-label="Toggle navigation">
@@ -15,22 +19,34 @@ export default function NavBar() {
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
                         <NavLink
-                            className="nav-item nav-link text-bg-primary p-3"
+                            className="nav-item nav-link text-white p-3 rounded-4"
                             to='/'
                         >
                             <i className="fa-solid fa-sign-hanging"></i> Estados
                         </NavLink>
                         <NavLink
-                            className="nav-item nav-link text-bg-primary p-3"
+                            className="nav-item nav-link text-white p-3"
                             to='/marcas'
                         >
                             <i className="fa-solid fa-tags"></i> Marcas
                         </NavLink>
                         <NavLink
-                            className="nav-item nav-link text-bg-primary p-3"
+                            className="nav-item nav-link text-white p-3"
+                            to='/tipoequipos'
+                        >
+                            <i className="fa-solid fa-walkie-talkie"></i> Tipos de Equipo
+                        </NavLink>
+                        <NavLink
+                            className="nav-item nav-link text-white p-3"
                             to='/usuarios'
                         >
-                            <i className="fa-solid fa-id-card-clip"></i> Tipos Equipos
+                            <i className="fa-solid fa-id-card-clip"></i> Usuarios
+                        </NavLink>
+                        <NavLink
+                            className="nav-item nav-link text-white p-3"
+                            to='/inventario'
+                        >
+                            <i className="fa-solid fa-envelopes-bulk"></i> Inventario
                         </NavLink>
 
                     </ul>
--- src/routes/MainRouter.js
+++ src/routes/MainRouter.js
@@ -3,6 +3,8 @@ import NavBar from "../components/userinterface/NavBar";
 import Estado from "../components/estado/Estado";
 import Usuarios from "../components/usuarios/Usuarios";
 import Marca from "../components/marca/Marca";
+import TipoEquipos from '../components/tipoequipos/TipoEquipos';
+import  Inventario from '../components/inventario/Inventario';
 import {BrowserRouter, Route ,Routes} from "react-router-dom";
 
 export default function MainRouter() {
@@ -13,6 +15,8 @@ export default function MainRouter() {
                 <Route path='/' element={<Estado />}/>
                 <Route path='/marcas' element={ <Marca />}/>
                 <Route path='/usuarios' element={ <Usuarios />}/>
+                <Route path='/tipoequipos' element={<TipoEquipos />}/>
+                <Route path='/inventario' element={<Inventario />}/>
             </Routes>
 
         </BrowserRouter>
